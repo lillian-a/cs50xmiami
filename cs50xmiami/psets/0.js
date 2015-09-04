7080e13296d4bf9b83de94e2399e6e96fea17060
Lillian Arguelles <Lillian@Lillians-MacBook-Pro.local>
2015-09-03 20:24:59 -0400

    Commit message

---
+++ cs50xmiami/psets/0.js
@@ -0,0 +1,19 @@
+/*
+The Fortune Teller
+
+Why pay a fortune teller when you can just program your fortune yourself?
+
+- Store the following into variables:
+  number of children, partner's name, geographic location, job title.
+
+- Output your fortune to the screen like so:
+  "You will be a X in Y, and married to Z with N kids."
+*/
+
+// write your solution here...
+
+var child = 2;
+var name = "A";
+var geo = "Washington DC";
+var job = "Cryptoanalyst";
+console.log("You will be a " + job + " in " + geo + ", and married to " + name + " with " + child + " kids.");
